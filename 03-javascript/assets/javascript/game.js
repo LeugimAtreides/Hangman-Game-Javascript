4028247ca6e4ed056c411d566c36a4df2a5f3a45
LeugimAtreides <m.villarreal789@hotmail.com>
2019-05-31 23:52:37 -0400

    Word Guess Game 3.5: Unfortunately it looks like the game will not be finished in time so this is probably my final build before submission, however I will continue working on it over the weekend to see how far I get. In this version I have added variables that define the user input based changes to the HTML file such as starting the game, and actually playing the game.

--- 03-javascript/assets/javascript/game.js
+++ 03-javascript/assets/javascript/game.js
@@ -4,6 +4,22 @@ $(document).readyState(function () {
     
     // The object will be called heroes and it will have hero names on the left and concurrent underscore spaces on the right
     var heroes = {
+        // Creating the matching hero names with their underscore counterpart
+        namesAndUnderscoredCounterParts = [
+            // the n character represents the name of the hero while the u character represents the spaces each letter takes up
+            {n:"SPIDERMAN", u:"---------"},
+            {n:"IRONMAN", u:"-------"},
+            {n:"HULK", u:"----"},
+            {n:"CAPTAINAMERICA", u:"--------------"},
+            {n:"ANTMAN", u:"------"},
+            {n:"BLACKWIDOW", u:"----------"}
+        ],
+
+        // creating the pool(array) of letters that the user will have available to them
+        userPoolOfLetters = [
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+        ],
+
         // creating the properties that will change with each user input
 
         lettersUsedSoFar: function() {
@@ -20,6 +36,29 @@ $(document).readyState(function () {
 
         guessTheHero: function() {
             // this will hold the function that plays the major components of the game
+            
+            let randomHero = namesAndUnderscoredCounterParts.u[Math.floor(Math.random()*namesAndUnderscoredCounterParts.u.length)];
+            // this declaration will allow a random hero name to start the game
+            
+            document.startGame.append(randomHero);
+            // this inserts the hero blanks into the paragraph element so the user can see it
+
+            document.keyup = function(event) {
+                // defines the event of the user pressing a letter
+
+                let userGuess = event.key;
+                // defines a variable that records the key that was pressed
+
+                let userGuessModified = userGuess.toUpperCase();
+                // defines the variable that changes the user input into a upper case character
+
+                if (userGuessModified == userPoolOfLetters.indexOf()) {
+                    // defines the situation where the user hits a key that matches the alphabet array
+                    
+                }
+            }
+
+
         },
         
         startGame: function() {
