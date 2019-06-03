19adb5abbabeb351206246ec270db0b18368401f
LeugimAtreides <m.villarreal789@hotmail.com>
2019-06-03 11:42:24 -0400

    Word Guess Game 3.5.1: copied the index.html file into the folder above it so that it will show up on the GitPages

--- 03-javascript/assets/javascript/game.js
+++ 03-javascript/assets/javascript/game.js
@@ -1,22 +1,17 @@
-$(document).readyState(function () {
 
-    // I will attempt to put everything inside of an object since it keep things tight and organized into "heroes"
+
+// I will attempt to put everything inside of an object since it keep things tight and organized into "heroes"
     
     // The object will be called heroes and it will have hero names on the left and concurrent underscore spaces on the right
     var heroes = {
         // Creating the matching hero names with their underscore counterpart
-        namesAndUnderscoredCounterParts = [
+        namesOf : [
             // the n character represents the name of the hero while the u character represents the spaces each letter takes up
-            {n:"SPIDERMAN", u:"---------"},
-            {n:"IRONMAN", u:"-------"},
-            {n:"HULK", u:"----"},
-            {n:"CAPTAINAMERICA", u:"--------------"},
-            {n:"ANTMAN", u:"------"},
-            {n:"BLACKWIDOW", u:"----------"}
+            "BLACK WIDOW", "CAPTAIN AMERICA", "SPIDER-MAN", "IRON MAN", "ANT MAN", "THE HULK",
         ],
 
         // creating the pool(array) of letters that the user will have available to them
-        userPoolOfLetters = [
+        userPoolOfLetters : [
             "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
         ],
 
@@ -37,8 +32,9 @@ $(document).readyState(function () {
         guessTheHero: function() {
             // this will hold the function that plays the major components of the game
             
-            let randomHero = namesAndUnderscoredCounterParts.u[Math.floor(Math.random()*namesAndUnderscoredCounterParts.u.length)];
+            let randomHero = this.namesAndUnderscoredCounterParts[Math.floor(Math.random()*namesAndUnderscoredCounterPartslength)];
             // this declaration will allow a random hero name to start the game
+            
             
             document.startGame.append(randomHero);
             // this inserts the hero blanks into the paragraph element so the user can see it
@@ -52,32 +48,34 @@ $(document).readyState(function () {
                 let userGuessModified = userGuess.toUpperCase();
                 // defines the variable that changes the user input into a upper case character
 
-                if (userGuessModified == userPoolOfLetters.indexOf()) {
+                for (let i = 0; i < namesAndUnderscoredCounterParts.length; i++) {
+
+                    // for each new UserGuessModified in the Array, we crete a new paragraph to hold that text
+                    const namesAndUnderscoredCounterPartsIndex = namesAndUnderscoredCounterParts[i];
+                    
+                }
+
+                if (userGuessModified == this.userPoolOfLetters.indexOf()) {
                     // defines the situation where the user hits a key that matches the alphabet array
                     this.userPoolOfLetters.splice(userGuessModified);
                     // defines the need for removing the used letter from the pool of letters
                     this.numberOfGuesses++;
-                    // increased the number of guesses value by one
-                    document.this.lettersLeft.innerHTML(this.userPoolOfLetters);
+                    // increases the number of guesses value by one
+                    document.lettersLeft.innerHTML(this.userPoolOfLetters);
+                    // modifies the HTML value of lettersLeft to include only the remaining letters of the array userPoolOfLetters
+                };
+
+                if (userGuessModified == this.namesAndUnderscoredCounterParts.n.indexOf()) {
+                    // this defines the user guessing a correct letter in the game
+
+
                 }
+
             }
 
 
         },
-        
-        startGame: function() {
-            // the first method will determine when the user starts the game
-            document.keyup = function(event) {
-                // the second method will assign the space bar as the key which begins the game
-                const userStartGame = event.key;
-                if (userStartGame === " ") {
-                    // The if statement defines that the if statement will cause the paragraph change when the user hits the space bar
-                    document.getElementById("#storyScreenText").innerHTML(
-                        this.guessTheHero + <br></br> + "Letters Left: " + this.lettersLeft++ + <br></br> + "Number of Guesses: " + this.numberOfGuesses++ + <br></br> + "Letters Used So Far: " + this.lettersUsedSoFar + <br></br>)
-                }
-            }
-        }
-        }
-    }
 
-)
+        
+        }
+    
